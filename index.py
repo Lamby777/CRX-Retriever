d23f043038207f464acb4adf7a5a9f3a5a28b584
Dex <sheepareawesome777@gmail.com>
2022-09-23 10:10:31 -0400

    :rocket: well boys we did it, errors are no more! :rocket:

--- index.py
+++ index.py
@@ -1,14 +1,15 @@
-from torpy import TorClient
-
 print("ID of Chrome Extension:")
 ext_id = input()
 
-url = "https://clients2.google.com/service/update2/crx?response=redirect&os=linux&arch=x64&os_arch=x86_64&nacl_arch=x86-64&prod=chromium&prodchannel=unknown&prodversion=91.0.4442.4&lang=en-US&acceptformat=crx2,crx3&x=id%3Dxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx%26installsource%3Dondemand%26uc"
+url = "https://clients2.google.com/service/update2/crx?response=redirect&os=linux&arch=x64&os_arch=x86_64&nacl_arch=x86-64&prod=chromium&prodchannel=unknown&prodversion=91.0.4442.4&lang=en-US&acceptformat=crx2,crx3&x=id%3D"+ext_id+"%26installsource%3Dondemand%26uc"
 
 from torpy.http.requests import TorRequests
+
+response = None
+
 with TorRequests() as tor_requests:
 	with tor_requests.get_session() as session:
 		response = session.get(url)
 
-with open("filename.txt", "wb") as f:
-	response.raw()
+with open(ext_id+".zip", "wb") as f:
+	f.write(response.raw())
